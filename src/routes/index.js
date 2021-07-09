404210797e58cf387c870012c82e389264c9ef25
arthur.gallina@gmail.com <arthur.gallina@gmail.com>
2021-07-09 18:01:34 -0300

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,55 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return <div style={{ background: 'blue', padding: '10px', height: '75vh'}}>Home</div>;
+}
+
+function About() {
+  return <div style={{ background: 'yellow', padding: '10px', height: '75vh'}}>About</div>;
+}
+
+function Users() {
+  return <div style={{ background: 'red', padding: '10px', height: '75vh'}}>Users</div>;
+}
