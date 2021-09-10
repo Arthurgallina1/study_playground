0e1d33ced7bb86a7aae7b621c848f9ebb58f6991
arthur.gallina@gmail.com <arthur.gallina@gmail.com>
2021-09-09 23:16:02 -0300

    add use location

--- src/hooks/useQueryParams.js
+++ src/hooks/useQueryParams.js
@@ -1,27 +1,35 @@
 import React, { useEffect, useState } from 'react'
+import { useLocation } from './useLocation'
+
 
 export default function useQueryParams() {
+  const { push, replace, pathname, search } = useLocation()
   const [parameters, setParameters] = useState(
-    Object.fromEntries(new URLSearchParams(window.location.search).entries())
+    Object.fromEntries(new URLSearchParams(search).entries())
   )
 
+  useEffect(() => {
+    getParams()
+  }, [search])
+
   const getParams = () => {
-    const urlSearchParams = new URLSearchParams(window.location.search)
+    const urlSearchParams = new URLSearchParams(search)
     const params = Object.fromEntries(urlSearchParams.entries())
     setParameters(params)
     return params
   }
 
   const addParam = (key, value) => {
-    const urlSearchParams = new URLSearchParams(window.location.search)
+    const urlSearchParams = new URLSearchParams(search)
     urlSearchParams.append(key, value)
     setParameters(Object.fromEntries(urlSearchParams.entries()))
     window.history.replaceState(null, null, '?' + urlSearchParams.toString())
+    push(`?${urlSearchParams.toString()}`)
   }
 
   // paramsType = { key: string, value: string } []
   const setParams = (params) => {
-    const urlSearchParams = new URLSearchParams(window.location.search)
+    const urlSearchParams = new URLSearchParams(search)
     for (let i = 0; i < params.length; i++) {
       const { key, value } = params[i]
       urlSearchParams.append(key, value)
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,29 +1,19 @@
 import { Button } from '@chakra-ui/button'
+import { Box, Grid } from '@chakra-ui/layout'
 import React, { useEffect } from 'react'
-import { useHistory } from 'react-router'
+import { useLocation } from '../hooks/useLocation'
 import useQueryParams from '../hooks/useQueryParams'
 
 export default function Home() {
   const { parameters, getParams, addParam, setParams } = useQueryParams()
-  const history = useHistory()
-
-  console.log('parameters', parameters)
-  // useEffect(() => {
-  //   console.log('parameters', parameters)
-  // }, [parameters])
-  // useEffect(() => {
-  //   const params = getParams()
-  //   console.log(params)
-  // }, [])
+  const { push, replace } = useLocation()
 
   return (
     <div style={{ background: 'red', padding: '10px', height: '90vh' }}>
       Home 123
-      <Button onClick={() => history.push('/users')}>Id 30</Button>
-      <Button onClick={() => history.replace('/users?id=20&user=bad')}>
-        Id 20
-      </Button>
-      <Button variant="solid" onClick={() => history.push('/users/pop?id=30')}>
+      <Button onClick={() => push('/users')}>Id 30</Button>
+      <Button onClick={() => replace('/users?id=20&user=bad')}>Id 20</Button>
+      <Button variant="solid" onClick={() => push('/users/pop?id=30')}>
         /pop
       </Button>
       <Button
@@ -46,6 +36,14 @@ export default function Home() {
       >
         setParams
       </Button>
+      <Grid>
+        <Box color="blue.900">{Object.entries(parameters).map(i => {
+          return <div>
+            <span>{i[0]} </span>
+            <span>{i[1]} </span>
+          </div>
+        })}</Box>
+      </Grid>
     </div>
   )
 }
